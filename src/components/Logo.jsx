a7335ffc55b1edd041f745d208a408b03c487e46
Carlos Eduardo Silva Pires <carlossilvaptu01@gmail.com>
2024-10-14 09:24:38 -0300

    Caminho da logo alterado

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
         <div>
             <div className="logo">
                 <div className="logo-icon">
-                    <img src="public/Logo-75x75.png" alt="logo" />
+                    <img src="/logo-75x75.png" alt="logo" />
                 </div>
             </div>
         </div>
