23ed801f7d27c6e3f1a025e1194b1f550550a74c
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-15 14:15:11 -0300

    fix

--- src/components/TelaOperador.jsx
+++ src/components/TelaOperador.jsx
@@ -1,7 +1,7 @@
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Layout, message, Modal, Popconfirm, Select, Space, Table } from 'antd';
-import React, { useEffect, useState } from 'react';
-import './TelaOperador.css';
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Layout, message, Modal, Popconfirm, Select, Space, Table, } from "antd";
+import React, { useEffect, useState } from "react";
+import "./TelaOperador.css";
 const { Header, Content, Footer } = Layout;
 const { Option } = Select;
 
@@ -9,42 +9,45 @@ const TelaOperador = () => {
   const [dataSource, setDataSource] = useState([]);
   const [editingUser, setEditingUser] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [form] = Form.useForm(); // Adicione isso na declaração de estado
-
 
   const fetchUsuarios = async () => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch('https://api.airsoftcontrol.com.br/api/admin/listar', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`, // Corrigido para usar crase
-        },
-      });
-  
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        "https://api.airsoftcontrol.com.br/api/admin/listar",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // Corrigido para usar crase
+          },
+        }
+      );
+
       if (!response.ok) {
-        throw new Error('Erro ao buscar usuários.');
+        throw new Error("Erro ao buscar usuários.");
       }
-  
+
       const data = await response.json();
-      setDataSource(data.map(user => ({
-        ...user,
-        key: user.id,
-        role: user.roles.length > 0 ? user.roles[0].name : ''
-      })));
+      setDataSource(
+        data.map((user) => ({
+          ...user,
+          key: user.id,
+          role: user.roles.length > 0 ? user.roles[0].name : "",
+        }))
+      );
     } catch (error) {
-      console.error('Erro ao buscar usuários:', error);
-      message.error('Erro ao carregar usuários.');
+      console.error("Erro ao buscar usuários:", error);
+      message.error("Erro ao carregar usuários.");
     }
   };
-  
 
   useEffect(() => {
     fetchUsuarios();
   }, []);
-  
+
   useEffect(() => {
     if (isEditing) {
       form.setFieldsValue(editingUser);
@@ -53,89 +56,96 @@ const TelaOperador = () => {
 
   const createUser = () => {
     setEditingUser({
-      id: '',
-      email: '',
-      senha: '',
-      operador: '',
-      nome: '',
-      cpf: '',
-      dataNascimento: '',
-      telefone: '',
-      telefoneEmergencia: '',
-      tipoSanguineo: '',
-      ocupacao: '',
-      statusOperador: 'ATIVO',
-      role: 'RECRUTA',
+      id: "",
+      email: "",
+      senha: "",
+      operador: "",
+      nome: "",
+      cpf: "",
+      dataNascimento: "",
+      telefone: "",
+      telefoneEmergencia: "",
+      tipoSanguineo: "",
+      ocupacao: "",
+      statusOperador: "ATIVO",
+      role: "RECRUTA",
     });
     setIsEditing(true);
   };
 
   const saveUser = async (values) => {
     try {
-      const token = localStorage.getItem('token');
-      const method = editingUser.id ? 'PUT' : 'POST';
-      const url = editingUser.id 
+      const token = localStorage.getItem("token");
+      const method = editingUser.id ? "PUT" : "POST";
+      const url = editingUser.id
         ? `https://api.airsoftcontrol.com.br/api/admin/atualizar/${editingUser.id}`
-        : 'https://api.airsoftcontrol.com.br/api/admin/cadastro';
-  
+        : "https://api.airsoftcontrol.com.br/api/admin/cadastro";
+
       const userData = {
         ...values,
         operador: Number(values.operador),
       };
-  
-      console.log('Dados enviados:', userData);
-  
+
+      console.log("Dados enviados:", userData);
+
       const response = await fetch(url, {
         method,
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(userData),
       });
-  
+
       const result = await response.text(); // Pode ser .json() se a API retornar JSON
-  
+
       if (!response.ok) {
-        throw new Error(result || 'Erro ao salvar usuário.');
+        throw new Error(result || "Erro ao salvar usuário.");
       }
-  
-      message.success(editingUser.id ? 'Usuário atualizado com sucesso!' : 'Usuário criado com sucesso!');
+
+      message.success(
+        editingUser.id
+          ? "Usuário atualizado com sucesso!"
+          : "Usuário criado com sucesso!"
+      );
       setIsEditing(false);
       fetchUsuarios();
     } catch (error) {
       console.error(error);
-      message.error(error.message || 'Erro ao salvar usuário.');
+      message.error(error.message || "Erro ao salvar usuário.");
     }
   };
-  
 
   const editUser = (record) => {
     console.log("Editando usuário:", record); // Verifique o que está sendo passado
     setEditingUser({ ...record }); // Cria uma nova referência para garantir a atualização correta do estado
     setIsEditing(true);
   };
-  
+
   const deleteUser = async (id) => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch(`https://api.airsoftcontrol.com.br/api/admin/remover/${id}`, { // Corrigido para usar crase
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`, // Corrigido para usar crase
-        },
-      });
-  
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `https://api.airsoftcontrol.com.br/api/admin/remover/${id}`,
+        {
+          // Corrigido para usar crase
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // Corrigido para usar crase
+          },
+        }
+      );
+
       if (!response.ok) {
-        throw new Error('Erro ao excluir usuário.');
+        throw new Error("Erro ao excluir usuário.");
       }
-  
-      message.success('Usuário excluído com sucesso!');
+
+      message.success("Usuário excluído com sucesso!");
       fetchUsuarios();
     } catch (error) {
       console.error(error);
-      message.error('Erro ao excluir usuário.');
+      message.error("Erro ao excluir usuário.");
     }
   };
 
@@ -149,97 +159,13 @@ const TelaOperador = () => {
 
   const columns = [
     {
-      title: 'Status',
-      dataIndex: 'statusOperador',
-      key: 'statusOperador',
-      sorter: (a, b) => a.statusOperador.localeCompare(b.statusOperador),
-      responsive: ['md'],
-      width: 100,
-    },
-    {
-      title: 'Nome',
-      dataIndex: 'nome',
-      key: 'nome',
-      sorter: (a, b) => a.nome.localeCompare(b.nome),
-      responsive: ['md'],
-      width: 150,
-    },
-    {
-      title: 'Operador',
-      dataIndex: 'operador',
-      key: 'operador',
-      sorter: (a, b) => a.operador - b.operador,
-      responsive: ['md'],
-      width: 100,
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-      sorter: (a, b) => a.email.localeCompare(b.email),
-      responsive: ['md'],
-      width: 250,
-    },
-    {
-      title: 'CPF',
-      dataIndex: 'cpf',
-      key: 'cpf',
-      sorter: (a, b) => a.cpf.localeCompare(b.cpf),
-      responsive: ['md'],
-      width: 150,
-    },
-    {
-      title: 'Data de Nascimento',
-      dataIndex: 'dataNascimento',
-      key: 'dataNascimento',
-      sorter: (a, b) => new Date(a.dataNascimento) - new Date(b.dataNascimento),
-      render: (text) => (text ? new Date(text).toLocaleDateString() : ''),
-      responsive: ['lg'],
-      width: 120,
-    },
-    {
-      title: 'Telefone',
-      dataIndex: 'telefone',
-      key: 'telefone',
-      responsive: ['xl'],
-      width: 160,
-    },
-    {
-      title: 'Telefone Emergência',
-      dataIndex: 'telefoneEmergencia',
-      key: 'telefoneEmergencia',
-      responsive: ['xl'],
-      width: 150,
-    },
-    {
-      title: 'Tipo Sanguíneo',
-      dataIndex: 'tipoSanguineo',
-      key: 'tipoSanguineo',
-      responsive: ['lg'],
-      width: 110,
-    },
-    {
-      title: 'Ocupação',
-      dataIndex: 'ocupacao',
-      key: 'ocupacao',
-      sorter: (a, b) => a.ocupacao.localeCompare(b.ocupacao),
-      responsive: ['lg'],
-      width: 150,
-    },
-    {
-      title: 'Role',
-      dataIndex: 'role',
-      key: 'role',
-      sorter: (a, b) => a.role.localeCompare(b.role),
-      responsive: ['md'],
-      width: 150,
-    },
-    {
-      title: 'Ações',
-      key: 'actions',
+      title: "Ações",
+      key: "actions",
       render: (text, record) => (
         <Space size="middle">
-          <Button type="primary" onClick={() => editUser(record)}>Editar</Button>
+          <Button type="primary" onClick={() => editUser(record)}>
+            Editar
+          </Button>
           <Popconfirm
             title="Tem certeza que deseja excluir?"
             onConfirm={() => deleteUser(record.id)}
@@ -250,77 +176,209 @@ const TelaOperador = () => {
           </Popconfirm>
         </Space>
       ),
-      fixed: 'right',
-      width: 190,
+      //fixed: "left",
+      width: 180,
+    },
+    {
+      title: "Status",
+      dataIndex: "statusOperador",
+      key: "statusOperador",
+      sorter: (a, b) => a.statusOperador.localeCompare(b.statusOperador),
+      responsive: ["xs"],
+      width: 100,
+    },
+    {
+      title: "Nome",
+      dataIndex: "nome",
+      key: "nome",
+      sorter: (a, b) => a.nome.localeCompare(b.nome),
+      responsive: ["xs"],
+      width: 150,
+    },
+    {
+      title: "Operador",
+      dataIndex: "operador",
+      key: "operador",
+      sorter: (a, b) => a.operador - b.operador,
+      responsive: ["xs"],
+      width: 100,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      sorter: (a, b) => a.email.localeCompare(b.email),
+      responsive: ["md"],
+      width: 250,
+    },
+    {
+      title: "CPF",
+      dataIndex: "cpf",
+      key: "cpf",
+      sorter: (a, b) => a.cpf.localeCompare(b.cpf),
+      responsive: ["md"],
+      width: 150,
+    },
+    {
+      title: "Data de Nascimento",
+      dataIndex: "dataNascimento",
+      key: "dataNascimento",
+      sorter: (a, b) => new Date(a.dataNascimento) - new Date(b.dataNascimento),
+      render: (text) => (text ? new Date(text).toLocaleDateString() : ""),
+      responsive: ["lg"],
+      width: 120,
+    },
+    {
+      title: "Telefone",
+      dataIndex: "telefone",
+      key: "telefone",
+      responsive: ["xl"],
+      width: 160,
+    },
+    {
+      title: "Telefone Emergência",
+      dataIndex: "telefoneEmergencia",
+      key: "telefoneEmergencia",
+      responsive: ["xl"],
+      width: 150,
+    },
+    {
+      title: "Tipo Sanguíneo",
+      dataIndex: "tipoSanguineo",
+      key: "tipoSanguineo",
+      responsive: ["lg"],
+      width: 110,
+    },
+    {
+      title: "Ocupação",
+      dataIndex: "ocupacao",
+      key: "ocupacao",
+      sorter: (a, b) => a.ocupacao.localeCompare(b.ocupacao),
+      responsive: ["lg"],
+      width: 150,
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+      sorter: (a, b) => a.role.localeCompare(b.role),
+      responsive: ["xs"],
+      width: 150,
     },
   ];
 
   return (
     <Layout>
-      <Header style={{ backgroundColor: '#001529', color: '#fff', textAlign: 'center', padding: '0 50px' }}>
-        <h1 style={{ color: '#fff' }}>Tela Operador</h1>
+      <Header
+        style={{
+          backgroundColor: "#001529",
+          color: "#fff",
+          textAlign: "center",
+          padding: "0 50px",
+        }}
+      >
+        <h1 style={{ color: "#fff" }}>Tela Operador</h1>
       </Header>
-      <Content style={{ padding: '0 20px', margin: '20px 0 20px 0'}}>
-  <div style={{ background: '#fff', padding: '24px', borderRadius: '8px', boxShadow: '0 2px 8px rgba(0,0,0,0.1)', height:'101vh'}}>
-    <h1 style={{ fontSize: '1.5em' }}>Operador</h1>
-    <p>Esta é a tela de operador.</p>
+      <Content style={{ padding: "0 20px", margin: "20px 0 20px 0", height:'200%' }}>
+        <div
+          style={{
+            background: "#fff",
+            padding: "24px",
+            borderRadius: "8px",
+            boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
+            height: "100%",
+          }}
+        >
+          <h1 style={{ fontSize: "1.5em" }}>Operador</h1>
+          <p>Esta é a tela de operador.</p>
 
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', marginBottom: '16px', height:'120vh'}}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Input
-          placeholder="Pesquisar por nome"
-          value={searchText}
-          onChange={onSearchChange}
-          style={{ flex: 1, maxWidth: '300px', marginRight: '16px' }} // Altera a largura para se ajustar ao espaço
-        />
-        <Button type="primary" icon={<PlusOutlined />} onClick={createUser} style={{ margin: '-130px -15px 0 0' }}>
-          Cadastrar Usuário
-        </Button>
-      </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "16px",
+              marginBottom: "16px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Input
+                placeholder="Pesquisar por nome"
+                value={searchText}
+                onChange={onSearchChange}
+                style={{ flex: 1, maxWidth: "300px", marginRight: "16px" }} // Altera a largura para se ajustar ao espaço
+              />
+              <Button
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={createUser}
+                style={{ margin: "-130px -15px 0 0" }}
+              >
+                Cadastrar Usuário
+              </Button>
+            </div>
 
-      <Table
-  columns={columns}
-  dataSource={filteredData}
-  scroll={{ x: false, y:'100%'}} // Permite que a tabela se ajuste à largura da tela
-  pagination={{ pageSize: 10}}
-  style={{ width: '100%', overflow:'auto'}} // Garante que a tabela ocupe 100% da largura do contêiner
-/>
+            <Table
+              columns={columns}
+              dataSource={filteredData}
+              scroll={{ x: false, y: "100%" }} // Permite que a tabela se ajuste à largura da tela
+              pagination={{ pageSize: 10 }}
+              style={{ width: "100%", overflow: "auto" }} // Garante que a tabela ocupe 100% da largura do contêiner
+            />
+          </div>
+        </div>
 
-
-    </div>
-  </div>
-</Content>
-<Content> 
-<Footer style={{ textAlign: 'center', height:''}}>
-        Gestão de Usuários ©2024
-      </Footer>
-</Content>
+        <Footer style={{ textAlign: "center", height: "20vh" }}>
+          Gestão de Usuários ©2024
+        </Footer>
+      </Content>
 
       <Modal
-        title={editingUser?.id ? 'Editar Usuário' : 'Cadastrar Usuário'}
+        title={editingUser?.id ? "Editar Usuário" : "Cadastrar Usuário"}
         open={isEditing}
         onCancel={() => setIsEditing(false)}
         footer={null}
       >
         <Form
-    form={form} // Adicione esta linha
-    initialValues={editingUser}
-    onFinish={saveUser}
-    layout="vertical"
->
-          <Form.Item label="Nome" name="nome" rules={[{ required: true, message: 'Por favor insira o nome!' }]}>
+          form={form} // Adicione esta linha
+          initialValues={editingUser}
+          onFinish={saveUser}
+          layout="vertical"
+        >
+          <Form.Item
+            label="Nome"
+            name="nome"
+            rules={[{ required: true, message: "Por favor insira o nome!" }]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Por favor insira o email!' }]}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Por favor insira o email!" }]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Senha" name="senha" rules={[{ required: true, message: 'Por favor insira a senha!' }]}>
+          <Form.Item
+            label="Senha"
+            name="senha"
+            rules={[{ required: true, message: "Por favor insira a senha!" }]}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item label="Operador" name="operador">
             <Input type="number" />
           </Form.Item>
-          <Form.Item label="CPF" name="cpf" rules={[{ required: true, message: 'Por favor insira o CPF!' }]}>
+          <Form.Item
+            label="CPF"
+            name="cpf"
+            rules={[{ required: true, message: "Por favor insira o CPF!" }]}
+          >
             <Input />
           </Form.Item>
           <Form.Item label="Data de Nascimento" name="dataNascimento">
@@ -343,14 +401,19 @@ const TelaOperador = () => {
               <Option value="AB-">AB-</Option>
               <Option value="O+">O+</Option>
               <Option value="O-">O-</Option>
-
             </Select>
           </Form.Item>
-          
+
           <Form.Item label="Ocupação" name="ocupacao">
             <Input />
           </Form.Item>
-          <Form.Item label="Role" name="role" rules={[{ required: true, message: 'Por favor selecione um role!' }]}>
+          <Form.Item
+            label="Role"
+            name="role"
+            rules={[
+              { required: true, message: "Por favor selecione um role!" },
+            ]}
+          >
             <Select>
               <Option value="RECRUTA">RECRUTA</Option>
               <Option value="OPERADOR">OPERADOR</Option>
@@ -358,17 +421,22 @@ const TelaOperador = () => {
             </Select>
           </Form.Item>
 
-          <Form.Item label="Status" name="statusOperador" rules={[{ required: true, message: 'Por favor selecione o papel!' }]}>
+          <Form.Item
+            label="Status"
+            name="statusOperador"
+            rules={[
+              { required: true, message: "Por favor selecione o papel!" },
+            ]}
+          >
             <Select>
               <Option value="ATIVO">ATIVO</Option>
               <Option value="INATIVO">INATIVO</Option>
             </Select>
           </Form.Item>
 
-
           <Form.Item>
             <Button type="primary" htmlType="submit">
-              {editingUser?.id ? 'Atualizar' : 'Cadastrar'}
+              {editingUser?.id ? "Atualizar" : "Cadastrar"}
             </Button>
           </Form.Item>
         </Form>
