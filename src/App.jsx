22486c75d30190d86375ef15c012c4654f8bbf36
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-15 15:45:42 -0300

    feat: add overflow menulist

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,51 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from './components/Logo';
 import MenuList from './components/MenuList';
-import AppRoutes from './Routes'; // Importando as rotas
+import AppRoutes from './Routes';
 
 const { Header, Sider, Content } = Layout;
 
-// Função para verificar se o usuário está autenticado
 const isAuthenticated = () => {
-  return !!localStorage.getItem('token'); // Verifica se o token está no localStorage
+  return !!localStorage.getItem('token');
 };
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(true);
   const [collapsed, setCollapsed] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false); // Para detectar telas menores que 1200px
   const navigate = useNavigate();
 
-  // Verifique se o usuário está autenticado ao carregar a aplicação
   useEffect(() => {
     if (!isAuthenticated()) {
-      navigate('/login'); // Redireciona para a página de login se não estiver autenticado
+      navigate('/login');
     }
   }, [navigate]);
 
-  // Função para deslogar com confirmação
+  // Verifica o tamanho da tela ao carregar a página
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 1200);
+    };
+
+    // Define o estado inicial
+    handleResize();
+
+    // Adiciona o evento para monitorar o redimensionamento
+    window.addEventListener('resize', handleResize);
+
+    // Limpa o evento ao desmontar o componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const handleLogout = () => {
     Modal.confirm({
       title: 'Deseja sair?',
       content: 'Tem certeza de que deseja deslogar?',
       onOk: () => {
-        localStorage.removeItem('token'); // Remove o token do localStorage
-        navigate('/login'); // Redireciona para a página de login
-      },
-      onCancel: () => {
-        // Ação opcional no cancelamento
+        localStorage.removeItem('token');
+        navigate('/login');
       },
     });
   };
@@ -52,18 +65,22 @@ function App() {
     <Layout style={{ minHeight: '100%' }}>
       {isAuthenticated() ? (
         <>
-          <Sider 
-            collapsed={collapsed} 
-            collapsible 
-            trigger={null} 
-            theme={darkTheme ? 'dark' : 'light'} 
+          <Sider
+            collapsed={collapsed}
+            collapsible
+            trigger={null}
+            theme={darkTheme ? 'dark' : 'light'}
             className='sidebar'
+            style={{
+              overflowY: isSmallScreen ? 'auto' : 'hidden', // Adiciona rolagem apenas em telas menores
+              maxHeight: '100vh',
+            }}
           >
             <Logo />
-            <MenuList darkTheme={darkTheme} onLogout={handleLogout}/>
+            <MenuList darkTheme={darkTheme} onLogout={handleLogout} />
           </Sider>
           <Layout>
-            <Header style={{ padding: 0, background: colorBgContainer, display: 'flex', justifyContent: 'space-between', alignItems: 'center', height:'60px' }}>
+            <Header style={{ padding: 0, background: colorBgContainer, display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '60px' }}>
               <div>
                 <Button
                   type="text"
@@ -72,21 +89,21 @@ function App() {
                   icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
                 />
               </div>
-              <Button type="primary" onClick={handleLogout} style={{ marginRight: '16px' }}> {/* Adiciona margem à direita */}
+              <Button type="primary" onClick={handleLogout} style={{ marginRight: '16px' }}>
                 Logout
               </Button>
             </Header>
-            <Content style={{ margin: '16px', overflow: 'auto', height:'100vh'}}>
-  <div style={{ padding: '24px', background: '#fff', borderRadius: '8px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)'}}>
-    <AppRoutes /> {/* Renderiza as rotas principais */}
-  </div>
-</Content>
-          </Layout >
+            <Content style={{ margin: '16px', overflow: 'auto', height: '100vh' }}>
+              <div style={{ padding: '24px', background: '#fff', borderRadius: '8px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)' }}>
+                <AppRoutes />
+              </div>
+            </Content>
+          </Layout>
         </>
       ) : (
-        <Layout style={{ padding: 0}}>
+        <Layout style={{ padding: 0 }}>
           <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <AppRoutes /> {/* Renderiza apenas o componente de login */}
+            <AppRoutes />
           </Content>
         </Layout>
       )}
